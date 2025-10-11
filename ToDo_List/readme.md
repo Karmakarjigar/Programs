9b6001ad47abb18d4fb168b7a6fe87f2d99b067a
Karmakarjigar <karmakarjigar@gmail.com>
2025-10-11 13:44:42 +0530

    Add changes

--- ToDo_List/readme.md
+++ ToDo_List/readme.md
@@ -3,5 +3,5 @@
 1. Add Simple Structure
 2. Add javascript
 3. Adding Item
-4. Doing changes through
+4. Doing changes through javascript
 5. fixing the div 'container'
--- ToDo_List/todolist.js
+++ ToDo_List/todolist.js
@@ -1,21 +1,4 @@
-// console.log('Hello World');
-
-// document.getElementById('AddItemBtn').addEventListener('click', function (event) {
-//     const data = document.getElementById('AddItemTextBox').value.trim()
-//     console.log(document.getElementById('AddItemTextBox').value)
-//     const item = `
-//                     <br>
-//                     <div class="data">
-//                     <label for="input1">
-//                         <input type="checkbox" id="inuput1">
-//                     </label>
-//                     ${data}
-//                     <span>❌</span>
-//                 </div>`
-//     document.getElementById('todoItems').innerHTML += item;
-// })
-
-document.getElementById('AddItemBtn').addEventListener('click', function (event){
+document.getElementById('AddItemBtn').addEventListener('click', function (event) {
     const data = document.getElementById('AddItemTextBox').value.trim()
     console.log(document.getElementById('AddItemTextBox').value)
     const item = `
@@ -29,4 +12,17 @@ document.getElementById('AddItemBtn').addEventListener('click', function (event)
                     </div>
     `
     document.getElementById('todoItems').innerHTML += item;
+    document.getElementById('AddItemTextBox').value = '';
 })
+
+document.querySelectorAll(".input .checkbox").forEach((element) => {
+    element.addEventListener('click', function(event) {
+        const item = this.parentNode;
+
+        if (this.checked) {
+            item.classList.add('strikethrough');
+        } else {
+            item.classList.remove('strikethrough');
+        }
+    });
+});
