9fca597045bc40d3db3c877cabb28b60b61554c9
Karmakarjigar <karmakarjigar@gmail.com>
2025-10-11 13:05:05 +0530

    Add changes

---
+++ ToDo_List/readme.md
@@ -0,0 +1,7 @@
+# To Do list
+
+1. Add Simple Structure
+2. Add javascript
+3. Adding Item
+4. Doing changes through
+5. fixing the div 'container'
--- ToDo_List/todolist.js
+++ ToDo_List/todolist.js
@@ -1,14 +1,32 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-document.getElementById('AddItemBtn').addEventListener('click', function (event) {
+// document.getElementById('AddItemBtn').addEventListener('click', function (event) {
+//     const data = document.getElementById('AddItemTextBox').value.trim()
+//     console.log(document.getElementById('AddItemTextBox').value)
+//     const item = `
+//                     <br>
+//                     <div class="data">
+//                     <label for="input1">
+//                         <input type="checkbox" id="inuput1">
+//                     </label>
+//                     ${data}
+//                     <span>❌</span>
+//                 </div>`
+//     document.getElementById('todoItems').innerHTML += item;
+// })
+
+document.getElementById('AddItemBtn').addEventListener('click', function (event){
     const data = document.getElementById('AddItemTextBox').value.trim()
     console.log(document.getElementById('AddItemTextBox').value)
-    const item = `<div class="data">
-                    <label for="input1">
-                        <input type="checkbox" id="inuput1">
-                    </label>
-                    ${data}
-                    <span>❌</span>
-                </div>`
+    const item = `
+                    <br>
+                        <div class="data">
+                        <label for="input1">
+                            <input type="checkbox" id="inuput1">
+                        </label>
+                        ${data}
+                        <span>❌</span>
+                    </div>
+    `
     document.getElementById('todoItems').innerHTML += item;
 })
